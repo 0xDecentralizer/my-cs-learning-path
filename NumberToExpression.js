02acbc3b0b13f1aedfd9d737c2e43a203c4a8b06
Mohammad Mahdi <mohammadmahdikeshavarz1382@gmail.com>
2025-06-02 18:59:22 +0330

    Add generateRandomExpression function to NumberToExpression.js

--- NumberToExpression.js
+++ NumberToExpression.js
@@ -3,3 +3,13 @@ const operators = ['+', '-', '*', '/'];
 function getRandomOperator() {
     const index = Math.floor(Math.random() * operators.length)
 }
+
+function generateRandomExpression() {
+    const a = Math.floor(Math.random() * 10);
+    const b = Math.floor(Math.random() * 10);
+    const op = getRandomOperator();
+
+    return `${a} ${op} ${b}`;
+}
+
+console.log(generateRandomExpression());
