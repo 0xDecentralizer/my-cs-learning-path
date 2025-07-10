11833469aa2e08e25be6d6a4e5a8c613498beb06
Mohammad Mahdi <mohammadmahdikeshavarz1382@gmail.com>
2025-07-10 18:06:59 +0330

    Add Chapter2Challenge4.js: Implement tip calculation logic for an array of bills, including total values and console output

--- Jonas Course Challenges/Chapter2Challenge4.js
+++ Jonas Course Challenges/Chapter2Challenge4.js
@@ -15,3 +15,18 @@ and total values (bill + tip) for every bill value in the bills array. Use a for
 TEST DATA: 22, 295, 176, 440, 37, 105, 10, 1100, 86, and 52.
 */
 
+const bills = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
+const tips = [];
+const totals = [];
+
+const calcTip = function(bill) {
+    for (let i = 0; i < bill.length; i++) {
+        const tip = bill[i] >= 50 && bill[i] <= 300 ? bill[i] * 0.15 : bill[i] * 0.2;
+        tips.push(tip);
+        totals[i] = Math.trunc(tip + bill[i]);
+        console.log(`Tip: ${tips[i]}, Bill: ${bill[i]}, Total value: ${totals[i]}
+            ------`);
+    }
+}
+
+calcTip(bills);
