9750317981b1b9437e53252b9bf89e91ea127f4d
Mohammad Mahdi <mohammadmahdikeshavarz1382@gmail.com>
2025-07-10 18:06:49 +0330

    Refactor Chapter2Challenge3.js: Improve BMI comparison output and streamline logic for better readability

---
+++ Jonas Course Challenges/Chapter2Challenge4.js
@@ -0,0 +1,17 @@
+/*
+023 CHALLENGE #4
+Instructions
+Let's improve Steven's tip calculator even more, this time using loops!
+
+Your tasks:
+
+- Create an array called bills containing all 10 test bill values.
+
+- Create empty arrays for the tips and the totals (tips and totals)
+
+- Use the calcTip function we wrote before (included in the starter code) to calculate tips 
+and total values (bill + tip) for every bill value in the bills array. Use a for loop to perform the 10 calculations!
+
+TEST DATA: 22, 295, 176, 440, 37, 105, 10, 1100, 86, and 52.
+*/
+
