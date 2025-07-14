0e5462f2e6662259aedc3b241d5ccec7f71a4474
Mohammad Mahdi <mohammadmahdikeshavarz1382@gmail.com>
2025-07-14 23:29:57 +0330

    Add Find the Missing Letter implementation: Create a new file with a function to identify the missing letter in a sequence of consecutive letters, including an example and a detailed description of the algorithm.

--- CodeWars/Find the missing letter.js
+++ CodeWars/Find the missing letter.js
@@ -1,6 +1,6 @@
 'use strict';
 /*
-Find the missing letter
+Find the missing letter: https://www.codewars.com/kata/5839edaa6754d6fec10000a2/javascript
 Write a method that takes an array of consecutive (increasing) letters as input and that returns the missing letter in the array.
 
 You will always get an valid array. And it will be always exactly one letter be missing. The length of the array will always be at least 2.
