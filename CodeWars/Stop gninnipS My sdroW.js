4be4a0d12ede3b15c50b771f564f28a4f2e4a55e
Mohammad Mahdi <mohammadmahdikeshavarz1382@gmail.com>
2025-07-15 10:41:27 +0330

    Add implementation for reversing words with five or more letters: Create a new function that processes a string, reversing words of sufficient length while maintaining the original order of shorter words. Include example usage and a detailed description of the algorithm.

---
+++ CodeWars/Stop gninnipS My sdroW.js
@@ -0,0 +1,30 @@
+'use strict';
+/*
+Stop gninnipS My sdroW: https://www.codewars.com/kata/5264d2b162488dc400000001/javascript
+Write a function that takes in a string of one or more words, and returns the same string,
+ but with all words that have five or more letters reversed (Just like the name of this Kata).
+ Strings passed in will consist of only letters and spaces. Spaces will be included only when more than one word is present.
+
+Examples:
+
+"Hey fellow warriors"  --> "Hey wollef sroirraw" 
+"This is a test        --> "This is a test" 
+"This is another test" --> "This is rehtona test"
+*/
+
+const sayHey = 'Hey great CodeWars keep going throw problems';
+const solution = (str) => {
+    const words = str.split(' ');
+    const finalString = [];
+    for (let i = 0; i < words.length; i++) {
+        if (words[i].length >= 5) {
+            const reversedWord = words[i].split('').reverse().join('');
+            finalString.push(reversedWord);
+        } else {
+            finalString.push(words[i]);
+        }
+    }
+
+    return finalString.join(' ');
+}
+console.log(solution(sayHey)); // O(n) complexity
