d02977405d3a3210f753f67704e3a3f9af201b03
Mohammad Mahdi <mohammadmahdikeshavarz1382@gmail.com>
2025-07-20 19:21:12 +0330

    Add initial setup for "Guess My Number" game: Define variables for random number, score, and high score, and implement random number generator function.

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,1 +1,12 @@
 'use strict';
+
+
+let randomNumber;
+let score = 0;
+let highScore = 0;
+
+const randomNumGenerator = function() {
+    return Math.floor(Math.random() * 20 + 1);
+}
+// randomNumber = randomNumGenerator();
+
