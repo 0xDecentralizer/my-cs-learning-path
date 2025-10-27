f7b9c1103ab7caee37390cbb22d5cd68e9d7c189
Mohammad Mahdi Keshavarz <MohammadMahdiKeshavarz1382@gmail.com>
2025-10-27 15:19:00 +0330

    feat: implement rolling functionality for dice in Pig Game

--- Jonas Course/07-Pig-Game/script.js
+++ Jonas Course/07-Pig-Game/script.js
@@ -15,3 +15,23 @@ score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
 
+// Rolling Functionality
+btnRoll.addEventListener('click', function() {
+    // Generate random number 
+    const dice = Math.floor(Math.random() * 6 + 1); 
+    
+    // Display it 
+    diceEl.classList.remove('hidden');
+    diceEl.setAttribute('src', `dice-${dice}.png`)
+    
+    // Check if it's 1, switch to other player
+    if (dice === 1) {
+        current0El.textContent = 0;
+        dice.setAttribute('box-shadow', '0 0rem 5rem rgba(255, 0, 0, 0.582)');
+        // dice. 
+    } else {
+        current0El.textContent = +current0El.textContent + dice;
+    }
+    // score0El.textContent = dice;
+    
+});
