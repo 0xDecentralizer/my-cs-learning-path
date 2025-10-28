afeb108c9324be2613d0214d717a449be6145134
Mohammad Mahdi Keshavarz <MohammadMahdiKeshavarz1382@gmail.com>
2025-10-28 15:01:20 +0330

    feat: enhance rolling functionality and improve game state handling in Pig Game

--- Jonas Course/07-Pig-Game/script.js
+++ Jonas Course/07-Pig-Game/script.js
@@ -41,6 +41,9 @@ const init = function() {
     player0El.classList.remove('player--winner');
     player1El.classList.remove('player--active'); 
     player0El.classList.add('player--active');
+    
+    document.getElementById('name--0').textContent = 'Player 1';
+    document.getElementById('name--1').textContent = 'Player 2';
 
 };
 init();
@@ -56,50 +59,45 @@ const switchPlayer = function() {
 
 // Rolling Functionality
 btnRoll.addEventListener('click', function() {
-    if (gameState === GameState.ENDED) {
+    if (gameState === GameState.PLAYING) {   
+        // Reset the dice shadow
+        diceEl.classList.remove('diceOne');
+        // Generate random number 
+        const dice = Math.floor(Math.random() * 6 + 1);
+        // Display it 
+        diceEl.classList.remove('hidden');
+        diceEl.setAttribute('src', `dice-${dice}.png`)
+        
+        // Check if it's 1, switch to other player
+        if (dice !== 1) {
+            currentScore += dice;
+            document.getElementById(`current--${currentPlayer}`).textContent = currentScore; 
+        } else {
+            switchPlayer();
+            diceEl.classList.add('diceOne');
+        }
+    } else { 
         alert('Start a new game =P');
-        breake;
-    }
-    
-    // Reset the dice shadow
-    diceEl.classList.remove('diceOne');
-    // Generate random number 
-    const dice = Math.floor(Math.random() * 6 + 1);
-    // Display it 
-    diceEl.classList.remove('hidden');
-    diceEl.setAttribute('src', `dice-${dice}.png`)
-    
-    // Check if it's 1, switch to other player
-    if (dice !== 1) {
-        currentScore += dice;
-        document.getElementById(`current--${currentPlayer}`).textContent = currentScore; 
-    } else {
-        switchPlayer();
-        diceEl.classList.add('diceOne');
     }
 });
 
 btnHold.addEventListener('click', function() {
-    if (gameState === GameState.ENDED) {
-        alert('Start a new game =P');
-        breake;
-    }
-    
-    scores[currentPlayer] += currentScore;
-    document.getElementById(`score--${currentPlayer}`).textContent = scores[currentPlayer];
-    if (scores[currentPlayer] >= 20) {
-        document.querySelector(`.player--${currentPlayer}`).classList.add('player--winner');
-        document.querySelector(`.player--${currentPlayer}`).classList.remove('player--active');
-        document.querySelector(`#name--${currentPlayer}`).textContent = `Player ${currentPlayer + 1} Wins`;
-        document.getElementById(`current--${currentPlayer}`).textContent = 0;  
-        gameState = GameState.ENDED;
-        diceEl.classList.add('hidden');
+    if (gameState === GameState.PLAYING) {
+        scores[currentPlayer] += currentScore;
+        document.getElementById(`score--${currentPlayer}`).textContent = scores[currentPlayer];
+        if (scores[currentPlayer] >= 20) {
+            document.querySelector(`.player--${currentPlayer}`).classList.add('player--winner');
+            document.querySelector(`.player--${currentPlayer}`).classList.remove('player--active');
+            document.querySelector(`#name--${currentPlayer}`).textContent = `Player ${currentPlayer + 1} Wins`;
+            document.getElementById(`current--${currentPlayer}`).textContent = 0;  
+            gameState = GameState.ENDED;
+            diceEl.classList.add('hidden');
+        } else {
+            switchPlayer();    
+        }
     } else {
-        switchPlayer();    
+        alert('Start a new game =P');
     }
 });
 
-btnNew.addEventListener('click', function() {
-    init();
-    document.getElementById(`name--${currentPlayer}`).textContent = `Player ${currentPlayer + 1}`;
-});
+btnNew.addEventListener('click', init);
